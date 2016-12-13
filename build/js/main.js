37d28da07239f498a1e4acaf1a47ea63f25ef460
AlexCSS <kalyuzhnyi@gmail.com>
2016-12-13 14:26:19 +0200

    init

---
+++ build/js/main.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".ba-main-menu__toggle"),n=document.getElementsByTagName("body")[0],a=document.querySelectorAll(".ba-overlay");e[0].onclick=function(){"ba-menu-open"===n.className?n.className="":n.className="ba-menu-open"},a[0].onclick=function(){n.className=""}}();
+//# sourceMappingURL=../sourcemaps/main.js.map
